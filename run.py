0ac099455424d1643f6f6028cd63e9606d1fd9cf
azimny86 <azimny86@gmail.com>
2023-05-19 14:04:33 +0000

    Add APIs and confirm access to the spreadsheet

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Configuration of access to Google Sheets
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+
+# Spread sheet name
+SHEET = GSPREAD_CLIENT.open('Bjj-Manager')
+
+
+members = SHEET.worksheet('sheet1')
+
+data = members.get_all_values()
+print(data)
