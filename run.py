8d3407ab566be3277eafd923817af877525950a3
azimny86 <azimny86@gmail.com>
2023-06-21 15:35:43 +0000

    Add main menu

--- run.py
+++ run.py
@@ -18,3 +18,49 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Bjj-Manager')
 
 
+def welcome():
+    while True:
+        print("==== BJJ Manager ====")
+        print("1. Add participant")
+        print("2. Remove participant")
+        print("3. Confirm payment")
+        print("9. About program")
+        print("Q. Exit the program")
+        print("=====================")
+        choice = input("Choose an option: ")
+
+        if choice == "1":
+            name = input("Enter participant's name: ")
+            group = input("Enter participant's group (Beginner/Advanced): ")
+            payment_confirmed = input("Has the payment been confirmed? (Yes/No): ").lower() == "yes"
+            add_participant(name, group, payment_confirmed)
+        elif choice == "2":
+            name = input("Enter participant's name to remove: ")
+            remove_participant(name)
+        elif choice == "3":
+            name = input("Enter participant's name to confirm payment: ")
+            confirm_payment(name)
+        elif choice == "9":
+            about()
+        elif choice == "q":
+            exit()
+        else:
+            print("Invalid option.")
+
+    print("Thank you for using BJJ Manager!")
+
+
+
+
+def clear():
+    """
+    Clear screen during game
+    """
+    os.system("clear")
+
+
+def main():
+    welcome()
+
+
+main()
